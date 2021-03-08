5b49b7ddf41a3d5f7a3044f5f7fda68eff50a03b
jatinudasi <jatin.udasi88@gmail.com>
2021-03-08 20:28:37 +0530

    vaulisten 8

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(function (err, req, res, next) {
   }
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.Port;
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
