03ea9beaf6ff0460a57878ef40eb60dbbd08d275
jatinudasi <jatin.udasi88@gmail.com>
2021-03-08 20:02:43 +0530

    valulisten

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
-require("dotenv").config();
+// require("dotenv").config();
 const bodyParser = require("body-parser");
 require("./database/mongodbinit");
 const app = express();
