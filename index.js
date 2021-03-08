a7e2aec9d8cf29b1c32bf5aa24433d7fb5b8f969
jatinudasi <jatin.udasi88@gmail.com>
2021-03-08 20:44:39 +0530

    vaul ??

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(function (err, req, res, next) {
   }
 });
 
-const PORT = process.env.$PORT;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
