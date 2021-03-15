cd6ae940a5cb0b9ea395db773202677ddea697ed
jatinudasi <jatin.udasi88@gmail.com>
2021-03-15 18:44:52 +0530

    filter in city

--- routes/class.routes.js
+++ routes/class.routes.js
@@ -731,4 +731,117 @@ app.get(
   }
 );
 
+app.get("/city/filter/:city", async (req, res, next) => {
+  try {
+    // let type = req.query.classtype;
+    // let city = req.query.city;
+
+    // classtype = type.split(",");
+    // cities = city.split(",");
+    // console.log(classtype);
+    // console.log(cities);
+
+    // let citiesarr = [];
+    // let classtypearr =[];
+
+    // for (let i = 0; i < cities.length; i++) {
+    // 	citiesarr.push({city:cities[i]});
+    // }
+
+    // for (let i = 0; i < classtype.length; i++) {
+    // 	classtypearr.push({classtype:classtype[i]});
+    // }
+
+    // console.log("cities",citiesarr);
+    // console.log("classtype",classtypearr)
+
+    // console.log(arr);
+
+    // let query = {$or: [...arr]}
+
+    //  let query ={$and:[{ activities:req.params.name},{$or:[{city:"Mumbai"},{city:"Pune"}]}]}
+    // const mycity =[];
+    // for(let i=0;i<req.query.citiesarr.length;i++){
+    // 	mycity.push({city:req.query.citiesarr[i]});
+    // }
+    // console.log(mycity)
+
+    // const classtypesjatin =[];
+    // for(let i=0;i<req.query.classtypearr.length;i++){
+    // 	classtypesjatin.push({classtype:req.query.classtypearr[i]});
+    // }
+    // console.log(classtypesjatin)
+
+    // if(req.query.citiesarr&&req.query.classtypearr)
+    // {
+    // console.log("both array given",req.query.citiesarr,req.query.classtypearr)
+    //  query ={$and:[{ activities:req.params.name},{$or:[...req.query.citiesarr]},{$or:[...req.query.classtypearr]}]}
+    // }
+    // else if(req.query.citiesarr){
+    // 	console.log("cities array given",req.query.citiesarr);
+    // 	query ={$and:[{ activities:req.params.name},{$or:[...req.query.citiesarr]}]}
+    // }else if(req.query.classtypearr){
+    // 	console.log("classtype array given",req.query.classtypearr);
+    // 	query ={$and:[{ activities:req.params.name},{$or:[...req.query.classtypearr]}]}
+    // }
+    // else{
+    // 	res.status(500).send("enter proper details")
+    // }
+
+    if (req.query.citiesarr && req.query.classtypearr) {
+      const mycity = [];
+      for (let i = 0; i < req.query.citiesarr.length; i++) {
+        mycity.push({ city: req.query.citiesarr[i] });
+      }
+      console.log(mycity);
+      const classtypesjatin = [];
+      for (let i = 0; i < req.query.classtypearr.length; i++) {
+        classtypesjatin.push({ classtype: req.query.classtypearr[i] });
+      }
+      console.log(classtypesjatin);
+      console.log(
+        "both array given",
+        req.query.citiesarr,
+        req.query.classtypearr
+      );
+      query = {
+        $and: [
+          { city: req.params.city },
+          { $or: [...mycity] },
+          { $or: [...classtypesjatin] },
+        ],
+      };
+    } else if (req.query.citiesarr) {
+      const mycity = [];
+      for (let i = 0; i < req.query.citiesarr.length; i++) {
+        mycity.push({ city: req.query.citiesarr[i] });
+      }
+      console.log(mycity);
+      console.log("cities array given", req.query.citiesarr);
+      query = { $and: [{ activities: req.params.name }, { $or: [...mycity] }] };
+    } else if (req.query.classtypearr) {
+      const classtypesjatin = [];
+      for (let i = 0; i < req.query.classtypearr.length; i++) {
+        classtypesjatin.push({ classtype: req.query.classtypearr[i] });
+      }
+      console.log(classtypesjatin);
+      console.log("classtype array given", req.query.classtypearr);
+      query = {
+        $and: [{ city: req.params.city }, { $or: [...classtypesjatin] }],
+      };
+    } else {
+      res.status(500).send("enter proper details");
+    }
+
+    //  query ={$and:[{ activities:req.params.name},{$or:[...citiesarr]},{$or:[...classtypearr]}]}
+    console.log("query", query);
+    const filter = await Class.find(query);
+    console.log(filter);
+
+    res.status(200).send({ classtype: filter });
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = app;
