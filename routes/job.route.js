3063848d9bf403336b81600de0a8a65cb08f524e
jatinudasi <jatin.udasi88@gmail.com>
2021-03-17 22:39:51 +0530

    for commit

--- routes/job.route.js
+++ routes/job.route.js
@@ -6,23 +6,61 @@ const role = require("./../helpers/role");
 const applicationController = require("../controllers/application.controller");
 const { upload, docupload } = require("./../helpers/multer");
 const { configcloud } = require("./../helpers/cloudinary");
+const Job = require("./../models/job.model");
 
-router.post("/image/:jobId", verifyaccesstoken, role.checkRole(role.ROLES.Recruiter), upload.single("image"), configcloud, jobsController.image);
+router.post(
+  "/image/:jobId",
+  verifyaccesstoken,
+  role.checkRole(role.ROLES.Recruiter),
+  upload.single("image"),
+  configcloud,
+  jobsController.image
+);
 
 router.get("/:jobId", verifyaccesstoken, jobsController.getOne);
 router.get("/", verifyaccesstoken, jobsController.get);
 router.post("/", verifyaccesstoken, jobsController.post);
 router.get("/applied", verifyaccesstoken, applicationController.fetchApplied);
-router.post("/myfile/:jobId", verifyaccesstoken, role.checkRole(role.ROLES.Recruiter), docupload.single("myFile"), configcloud, jobsController.myfile);
+router.post(
+  "/myfile/:jobId",
+  verifyaccesstoken,
+  role.checkRole(role.ROLES.Recruiter),
+  docupload.single("myFile"),
+  configcloud,
+  jobsController.myfile
+);
 router.get("/:city", verifyaccesstoken, jobsController.getbyCity);
 router.put("/:jobId", verifyaccesstoken, jobsController.putOne);
 router.delete("/:jobId", verifyaccesstoken, jobsController.deleteOne);
 router.post("/:jobId/apply", verifyaccesstoken, applicationController.apply);
 router.post("/:jobId/save", verifyaccesstoken, applicationController.save);
-router.post("/applicant/list", verifyaccesstoken, role.checkRole(role.ROLES.Recruiter), jobsController.list);
-router.post("/applicant/list/accepted", verifyaccesstoken, role.checkRole(role.ROLES.Recruiter), jobsController.accepted);
-router.post("/applicant/list/rejected", verifyaccesstoken, role.checkRole(role.ROLES.Recruiter), jobsController.rejected);
+router.post(
+  "/applicant/list",
+  verifyaccesstoken,
+  role.checkRole(role.ROLES.Recruiter),
+  jobsController.list
+);
+router.post(
+  "/applicant/list/accepted",
+  verifyaccesstoken,
+  role.checkRole(role.ROLES.Recruiter),
+  jobsController.accepted
+);
+router.post(
+  "/applicant/list/rejected",
+  verifyaccesstoken,
+  role.checkRole(role.ROLES.Recruiter),
+  jobsController.rejected
+);
+router.get("/filter/job/:activities", verifyaccesstoken, (req, res, next) => {
+  try {
 
+      const activitie = await Job.find({activities:req.params.activities});
+      res.status(200).send({activitie})
+  } catch (error) {
+    next(error);
+  }
+});
 /* router.get('/:jobId/details', verifyaccesstoken, applicationController.getApplicationDetails)
 //router.post('/:jobId/unsave', verifyaccesstoken, applicationController.unsave)
 
