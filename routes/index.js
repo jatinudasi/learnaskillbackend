895978ad18b0c9052c7acffe2a1c4a4592e9c76d
jatinudasi <jatin.udasi88@gmail.com>
2021-03-29 21:06:51 +0530

    comm

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,14 @@
-// const express = require("express");
-// const app = express.Router();
+const express = require("express");
+const app = express.Router();
 
-// const authroutes = require("./auth.route");
-// const jobroutes = require("./job.route");
-// const classroutes = require("./class.routes");
+const authroutes = require("./auth.route");
+const jobroutes = require("./job.route");
+const classroutes = require("./class.routes");
 
+// app.use("/applicant", authroutes);
+// app.use('/recruiter', authroutes);
+app.use("/auth", authroutes);
+app.use("/job", jobroutes);
+app.use("/class", classroutes);
 
-// // app.use("/applicant", authroutes);
-// // app.use('/recruiter', authroutes);
-// app.use('/auth',authroutes);
-// app.use('/job', jobroutes);
-// app.use('/class',classroutes);
-
-
-
-
-
-// module.exports = app;
+module.exports = app;
