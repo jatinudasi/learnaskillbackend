db1045c72397d48b69bce4f4f5f4b1b3881286aa
jatinudasi <jatin.udasi88@gmail.com>
2021-03-31 20:59:56 +0530

    comm for the day

--- models/class.model.js
+++ models/class.model.js
@@ -1,89 +1,88 @@
-// const { Schema, model } = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-// const classSchema = Schema({
-//   firstname: {
-//     type: String,
-//     required: true,
-//   },
-//   lastname: {
-//     type: String,
-//     required: true,
-//   },
-//   classowner: {
-//     type: Schema.Types.ObjectId,
-//     ref: "Recruiter"
-//   },
-//   image: {
-//     type: String
-//   },
-//   activities: {
-//     type: String,
-//     required: true,
-//     enum: [
-//       "Hospital",
-//       "Technical",
-//       "Sport",
-//       "Cenimatics",
-//       "Cooking",
-//       "Performance",
-//       "Programming",
-//     ]
-//   },
-//   category: {
-//     type: String,
-//     required: true,
-//   },
-//   classtype: {
-//     type: String,
-//     required: true,
-//     enum: ["Fulltime","Parttime","Remote"]
-//   },
-//   classname: {
-//     type: String,
-//     required: true
-//   },
-//   address: {
-//     type: String,
-//     required: true,
-//   },
-//   city: {
-//     type: String,
-//     required: true,
-//   },
-//   fees: {
-//     type: Number,
-//     required: true,
-//   },
-//   duration: {
-//     type: Number,
-//     required: true
-//   },
-//   vacancy: {
-//     type: Number,
-//     min: [1, "enter minimum vacancy 1"],
-//     max: [10, "enter max vacancy of 10"],
-//   },
-//   classinformation: {
-//     type: String
-//   },
-//   classdescription: {
-//     type: String
-//   },
-//   skills: {
-//     type: String
-//   },
-// });
+const classSchema = Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  classowner: {
+    type: Schema.Types.ObjectId,
+    ref: "Recruiter",
+  },
+  image: {
+    type: String,
+  },
+  activities: {
+    type: String,
+    required: true,
+    enum: [
+      "Hospital",
+      "Technical",
+      "Sport",
+      "Cenimatics",
+      "Cooking",
+      "Performance",
+      "Programming",
+    ],
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  classtype: {
+    type: String,
+    required: true,
+    enum: ["Fulltime", "Parttime", "Remote"],
+  },
+  classname: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  fees: {
+    type: Number,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  vacancy: {
+    type: Number,
+    min: [1, "enter minimum vacancy 1"],
+    max: [10, "enter max vacancy of 10"],
+  },
+  classinformation: {
+    type: String,
+  },
+  classdescription: {
+    type: String,
+  },
+  skills: {
+    type: String,
+  },
+});
 
+//  classSchema.pre('remove',async function(next){
+//    try {
+//      console.log("delete function triggered great");
 
-// //  classSchema.pre('remove',async function(next){
-// //    try {
-// //      console.log("delete function triggered great");
+//      console.log("value of this is :",this)
 
-// //      console.log("value of this is :",this)
-     
-// //    } catch (error) {
-// //      next(error);
-// //    }
-// //  })
+//    } catch (error) {
+//      next(error);
+//    }
+//  })
 
-// module.exports = model("Class", classSchema);
+module.exports = model("Class", classSchema);
