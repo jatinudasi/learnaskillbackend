01df0063a027eb22257e8612b1d08ab85c350537
jatinudasi <jatin.udasi88@gmail.com>
2021-04-19 22:06:54 +0530

    com for he day

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-// const express = require("express");
-// const cors = require("cors");
-// const logger = require("morgan");
-// require("dotenv").config();
-// const bodyParser = require("body-parser");
-// require("./database/mongodbinit");
-// const app = express();
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+require("./database/mongodbinit");
+const app = express();
 
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(cors());
-// app.use(express.json());
-// app.use(logger("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(logger("dev"));
 
-// const allroutes = require("./routes/index");
+const allroutes = require("./routes/index");
 
-// app.use(allroutes);
+app.use(allroutes);
 
-// app.get("/", (req, res, next) => {
-//   res.status(200).send("hello world");
-// });
+app.get("/", (req, res, next) => {
+  res.status(200).send("hello world");
+});
 
-// /** catch 404 and forward to error handler */
-// app.use("*", (req, res) => {
-//   return res.status(404).json({
-//     success: false,
-//     message: "API endpoint doesnt exist",
-//   });
-// });
+/** catch 404 and forward to error handler */
+app.use("*", (req, res) => {
+  return res.status(404).json({
+    success: false,
+    message: "API endpoint doesnt exist",
+  });
+});
 
-// app.use(function (err, req, res, next) {
-//   if (err.message) {
-//     res.status(400).send(err.message);
-//   } else {
-//     err.message = "internal server error";
-//     res.status(500).send(err.message);
-//   }
-// });
+app.use(function (err, req, res, next) {
+  if (err.message) {
+    res.status(400).send(err.message);
+  } else {
+    err.message = "internal server error";
+    res.status(500).send(err.message);
+  }
+});
 
-// const PORT = process.env.PORT || 8000;
-// app.listen(PORT, () => {
-//   console.log(`http://localhost:${PORT}`);
-// });
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
