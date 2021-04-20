8c60a8a86ee46eafe4abe1f107bea3964e944e82
jatinudasi <jatin.udasi88@gmail.com>
2021-04-20 23:03:07 +0530

    comm for the day

--- models/application.model.js
+++ models/application.model.js
@@ -1,40 +1,40 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
 
-const applicationSchema = new Schema(
-	{
-		jobId: {
-			type: Schema.Types.ObjectId,
-			ref: "Job",
-			required: true,
-		},
-		applicantId: {
-			type: Schema.Types.ObjectId,
-			ref: "Applicant",
-			required: true,
-		},
-		recruiterId: {
-			type: Schema.Types.ObjectId,
-			ref: "Recruiter",
-			required: true,
-		},
+// const applicationSchema = new Schema(
+// 	{
+// 		jobId: {
+// 			type: Schema.Types.ObjectId,
+// 			ref: "Job",
+// 			required: true,
+// 		},
+// 		applicantId: {
+// 			type: Schema.Types.ObjectId,
+// 			ref: "Applicant",
+// 			required: true,
+// 		},
+// 		recruiterId: {
+// 			type: Schema.Types.ObjectId,
+// 			ref: "Recruiter",
+// 			required: true,
+// 		},
 
-		resume: {
-			type: String,
-			required: true,
-		},
-		cover_letter: {
-			type: String,
-		},
-		status: {
-			type: String,
-			enum: ["Applied", "Confirmed", "Rejected"],
-			default: "Applied",
-		},
-	},
-	{
-		timestamps: true,
-	}
-);
+// 		resume: {
+// 			type: String,
+// 			required: true,
+// 		},
+// 		cover_letter: {
+// 			type: String,
+// 		},
+// 		status: {
+// 			type: String,
+// 			enum: ["Applied", "Confirmed", "Rejected"],
+// 			default: "Applied",
+// 		},
+// 	},
+// 	{
+// 		timestamps: true,
+// 	}
+// );
 
-module.exports = mongoose.model("Application", applicationSchema);
+// module.exports = mongoose.model("Application", applicationSchema);
