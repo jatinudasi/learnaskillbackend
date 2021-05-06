455f74d5178e59dd96940ac3941d3c1e52dcfa6e
jatinudasi <jatin.udasi88@gmail.com>
2021-05-06 23:55:51 +0530

    savejobmodel

--- models/savejob.model.js
+++ models/savejob.model.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
 
-const saveJobSchema = new Schema({
-  jobId: {
-    type: Schema.Types.ObjectId,
-    ref: "Job",
-    required: true,
-  },
-  applicantId: {
-    type: Schema.Types.ObjectId,
-    ref: "Applicant",
-    required: true,
-  },
-});
+// const saveJobSchema = new Schema({
+//   jobId: {
+//     type: Schema.Types.ObjectId,
+//     ref: "Job",
+//     required: true,
+//   },
+//   applicantId: {
+//     type: Schema.Types.ObjectId,
+//     ref: "Applicant",
+//     required: true,
+//   },
+// });
 
-module.exports = mongoose.model("SaveJob", saveJobSchema);
+// module.exports = mongoose.model("SaveJob", saveJobSchema);
