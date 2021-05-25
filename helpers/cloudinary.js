4dcc137628755764bd9163f7d78a25c9df0e2353
jatinudasi <jatin.udasi88@gmail.com>
2021-05-25 23:38:32 +0530

    cloudinary image upload

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -1,79 +1,79 @@
-// const cloudinary = require("cloudinary").v2;
-// require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
 
-// const configcloud = (req, res, next) => {
-//   cloudinary.config({
+const configcloud = (req, res, next) => {
+  cloudinary.config({
+    cloud_name: process.env.Cloudname,
+    api_key: process.env.Apikey,
+    api_secret: process.env.Apisecret,
+  });
+  next();
+};
+
+const uploadtocloud = (file) => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(file, function (error, image) {
+      console.log("file", file);
+      console.log(error);
+      console.log(image);
+      const fs = require("fs");
+      fs.unlinkSync(file);
+      console.log("image", image);
+      if (error) {
+        console.log("there is error ");
+        reject(new Error("there is error in cloudinary"));
+      }
+      resolve({ url: image.url, id: image.public_id });
+    });
+  });
+};
+
+const doctocloud = (file) => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(file, function (error, doc) {
+      console.log("file", file);
+      console.log(error);
+      console.log(doc);
+      const fs = require("fs");
+      fs.unlinkSync(file);
+      console.log("Resume", doc);
+      if (error) {
+        console.log("there is error ");
+        reject(new Error("there is error in cloudinary"));
+      }
+      resolve({ url: doc.url, id: doc.public_id });
+    });
+  });
+};
+module.exports = { configcloud, uploadtocloud, doctocloud };
+
+// const { config, uploader } = require("cloudinary");
+
+// const cloudinaryConfig = (req, res, next) => {
+//   config({
 //     cloud_name: process.env.Cloudname,
 //     api_key: process.env.Apikey,
 //     api_secret: process.env.Apisecret,
 //   });
 //   next();
+
 // };
 
-// const uploadtocloud = (file) => {
-//   return new Promise((resolve, reject) => {
-//     cloudinary.uploader.upload(file, function (error, image) {
-//       console.log("file", file);
-//       console.log(error);
-//       console.log(image);
-//       const fs = require("fs");
-//       fs.unlinkSync(file);
-//       console.log("image", image);
-//       if (error) {
-//         console.log("there is error ");
-//         reject(new Error("there is error in cloudinary"));
-//       }
-//       resolve({ url: image.url, id: image.public_id });
-//     });
-//   });
-// };
-
-// const doctocloud = (file) => {
-//   return new Promise((resolve, reject) => {
-//     cloudinary.uploader.upload(file, function (error, doc) {
-//       console.log("file", file);
-//       console.log(error);
-//       console.log(doc);
-//       const fs = require("fs");
-//       fs.unlinkSync(file);
-//       console.log("Resume", doc);
-//       if (error) {
-//         console.log("there is error ");
-//         reject(new Error("there is error in cloudinary"));
-//       }
-//       resolve({ url: doc.url, id: doc.public_id });
-//     });
-//   });
-// };
-// module.exports = { configcloud, uploadtocloud, doctocloud };
-
-// // const { config, uploader } = require("cloudinary");
+// module.exports = { cloudinaryConfig, uploader };
 
 // // const cloudinaryConfig = (req, res, next) => {
-// //   config({
-// //     cloud_name: process.env.Cloudname,
-// //     api_key: process.env.Apikey,
-// //     api_secret: process.env.Apisecret,
-// //   });
-// //   next();
+// // config({
+// // cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+// // api_key: process.env.CLOUDINARY_API_KEY,
+// // api_secret: process.env.CLOUDINARY_API_SECRET,
+// // });
+// // next();
+// // }
 
-// // };
-
-// // module.exports = { cloudinaryConfig, uploader };
-
-// // // const cloudinaryConfig = (req, res, next) => {
-// // // config({
-// // // cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-// // // api_key: process.env.CLOUDINARY_API_KEY,
-// // // api_secret: process.env.CLOUDINARY_API_SECRET,
-// // // });
-// // // next();
-// // // }
-
-// // exports.uploads = (file) =>{
-// //   return new Promise(resolve => {
-// //   cloudinary.uploader.upload(file, (result) =>{
-// //   resolve({url: result.url, id: result.public_id})
-// //   })
-// //   })
-// //   }
+// exports.uploads = (file) =>{
+//   return new Promise(resolve => {
+//   cloudinary.uploader.upload(file, (result) =>{
+//   resolve({url: result.url, id: result.public_id})
+//   })
+//   })
+//   }
